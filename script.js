fdc45113339579bad667411162f7d741540199d9
bass-dandy <bass-dandy@users.noreply.github.com>
2020-05-20 08:38:00 +0000

    deploy: 76359e25af2a824aa88ac48eb6b2c823acc11a4b

---
+++ script.js
@@ -0,0 +1,25 @@
+const nightModeToggle = document.querySelector('#night-mode-toggle input');
+
+// true if system dark mode preference was detected in earlier script
+nightModeToggle.checked = document.body.classList.contains('night-mode');
+
+nightModeToggle.addEventListener('change', (e) => {
+	e.target.checked
+		? document.body.classList.add('night-mode')
+		: document.body.classList.remove('night-mode');
+});
+
+const songs = document.querySelectorAll('#songs > li');
+
+songs.forEach((song) => {
+	const defaultChecked = localStorage.getItem(song.id) === 'true';
+	song.setAttribute('data-checked', defaultChecked);
+
+	const checkbox = song.querySelector('input');
+	checkbox.checked = defaultChecked;
+
+	checkbox.addEventListener('change', (e) => {
+		song.setAttribute('data-checked', e.target.checked);
+		localStorage.setItem(song.id, e.target.checked);
+	});
+});
