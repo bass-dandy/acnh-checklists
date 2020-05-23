bfbe843e1afa4c021f670fe5b18ac9a67d45a374
bass-dandy <ctdinh93@gmail.com>
2020-05-23 01:39:14 -0400

    wrap script in iife

--- static/script.js
+++ static/script.js
@@ -1,58 +1,61 @@
-// init night mode toggle
-const nightModeToggle = document.querySelector('#night-mode-toggle');
+(() => {
+	// init night mode toggle
+	const nightModeToggle = document.querySelector('#night-mode-toggle');
 
-function toggleNightMode() {
-	if (nightModeToggle.getAttribute('aria-checked') === 'false') {
-		nightModeToggle.setAttribute('aria-checked', 'true');
-		document.documentElement.classList.add('night-mode')
-	} else {
-		nightModeToggle.setAttribute('aria-checked', 'false');
-		document.documentElement.classList.remove('night-mode');
-	}
-}
+	const toggleNightMode = () => {
+		if (nightModeToggle.getAttribute('aria-checked') === 'false') {
+			nightModeToggle.setAttribute('aria-checked', 'true');
+			document.documentElement.classList.add('night-mode');
+		} else {
+			nightModeToggle.setAttribute('aria-checked', 'false');
+			document.documentElement.classList.remove('night-mode');
+		}
+	};
 
-nightModeToggle.addEventListener('keydown', (e) => {
-	if (e.key === ' ' || e.key === 'Enter') {
-		toggleNightMode();
-	}
-});
-
-nightModeToggle.addEventListener('click', toggleNightMode);
-
-document.addEventListener('keydown', (e) => {
-	if (e.key === 'Tab') {
-		document.documentElement.classList.add('keyboard-accessible');
-	}
-});
-document.addEventListener('mousedown', () => {
-	document.documentElement.classList.remove('keyboard-accessible');
-});
-
-// init tab panel
-const tabPanel = document.getElementById('tab-panel');
-const tabs = tabPanel.querySelectorAll('.tabs .tab');
-
-tabs.forEach((tab) => {
-	tab.addEventListener('click', (e) => {
-		tabPanel.setAttribute('data-selected-tab', e.target.dataset.tabId);
+	nightModeToggle.addEventListener('keydown', (e) => {
+		if (e.key === ' ' || e.key === 'Enter') {
+			toggleNightMode();
+		}
 	});
-});
 
-// init checklists
-tabs.forEach((tab) => {
-	const checklistItems = document.querySelectorAll(`#${tab.dataset.tabId} > li`);
+	nightModeToggle.addEventListener('click', toggleNightMode);
 
-	checklistItems.forEach((item) => {
-		const itemId = item.dataset.itemId;
-		const defaultChecked = localStorage.getItem(itemId) === 'true';
-		item.setAttribute('data-checked', defaultChecked);
+	// init reasonable focus outlines
+	document.addEventListener('keydown', (e) => {
+		if (e.key === 'Tab') {
+			document.documentElement.classList.add('keyboard-accessible');
+		}
+	});
+	document.addEventListener('mousedown', () => {
+		document.documentElement.classList.remove('keyboard-accessible');
+	});
 
-		const checkbox = item.querySelector('input');
-		checkbox.checked = defaultChecked;
+	// init tab panel
+	const tabPanel = document.getElementById('tab-panel');
+	const tabs = tabPanel.querySelectorAll('.tabs .tab');
 
-		checkbox.addEventListener('change', (e) => {
-			item.setAttribute('data-checked', e.target.checked);
-			localStorage.setItem(itemId, e.target.checked);
+	tabs.forEach((tab) => {
+		tab.addEventListener('click', (e) => {
+			tabPanel.setAttribute('data-selected-tab', e.target.dataset.tabId);
 		});
 	});
-})
+
+	// init checklists
+	tabs.forEach((tab) => {
+		const checklistItems = document.querySelectorAll(`#${tab.dataset.tabId} > li`);
+
+		checklistItems.forEach((item) => {
+			const itemId = item.dataset.itemId;
+			const defaultChecked = localStorage.getItem(itemId) === 'true';
+			item.setAttribute('data-checked', defaultChecked);
+
+			const checkbox = item.querySelector('input');
+			checkbox.checked = defaultChecked;
+
+			checkbox.addEventListener('change', (e) => {
+				item.setAttribute('data-checked', e.target.checked);
+				localStorage.setItem(itemId, e.target.checked);
+			});
+		});
+	});
+})();
