b3f65d0a7d547ebc2de71fe685b1783612785714
bass-dandy <ctdinh93@gmail.com>
2020-07-04 17:16:35 -0400

    add raw sea creatures data

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,7 @@
 module.exports = {
 	fish: require('./fish.json'),
 	bugs: require('./bugs.json'),
+	seaCreatures: require('./sea-creatures.json'),
 	fossils: require('./fossils.json'),
 	art: require('./art.json'),
 	songs: require('./songs.json'),
