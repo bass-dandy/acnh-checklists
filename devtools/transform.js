f522261821021c5eeb0c35b44ee09d7f6e21103f
bass-dandy <ctdinh93@gmail.com>
2020-07-04 17:17:03 -0400

    transform sea creatures data

--- devtools/transform.js
+++ devtools/transform.js
@@ -1,21 +1,86 @@
 const fs = require('fs');
 const data = require('../src/data');
+const util = require('./util');
 
-function transformTime(time) {
-	return time.split(', ');
+const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+
+function titleCase(str) {
+	return str
+		.split(' ')
+		.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+		.join(' ');
 }
 
-['fish', 'bugs'].forEach((key) => {
-	const listData = {...data[key]};
+function formatDisplayMonths(displayMonths) {
+	 return displayMonths.toLowerCase() === 'year-round' ? ['all year'] : displayMonths
+		.split(',')
+		.map((range) => {
+			return range.trim().split('-').map((month) => month.substring(0, 3)).join(' - ');
+		});
+}
 
-	Object.keys(data[key]).forEach((prop) => {
-		const item = data[key][prop];
+function getActiveMonths(displayMonths) {
+	if (displayMonths[0] === 'all year') {
+		return null;
+	}
+	const activeMonths = [];
+	displayMonths.forEach((range) => {
+		const [start, end] = range.split(' - ');
 
-		if (!item.name) {
-			throw new Error('JSON data missing [name] field');
+		let idx = months.indexOf(start);
+		while (months[idx] !== end) {
+			activeMonths.push(months[idx]);
+			idx = (idx + 1) % months.length;
 		}
-		listData[prop].location = item.location.toLowerCase();
+		activeMonths.push(end);
+	});
+	return activeMonths;
+}
+
+const creatureNames = Object.keys(data.seaCreatures).sort();
+const jsonData = {};
+
+creatureNames.forEach((name) => {
+	const src = data.seaCreatures[name];
+	const id = util.nameToId(name);
+
+	jsonData[id] = {
+		name: titleCase(name),
+		price: parseInt(src.price.replace(',', '')),
+		time: src.time.toLowerCase() === 'all day' ? ['All day'] : src.time
+			.replace(/ /g, '')
+			.replace(/a.m./g, ' AM')
+			.replace(/p.m./g, ' PM')
+			.replace(/-/g, ' - ')
+			.split(',')
+	};
+
+	const seasons = src.month.split('/').map((season) => season.toLowerCase().trim());
+
+	seasons.forEach((season) => {
+		if (season.includes('(northern)')) {
+			jsonData[id].nSeasonality = {
+				displayMonths: season.replace('(northern)', '').trim()
+			};
+		} else if (season.includes('(southern)')) {
+			jsonData[id].sSeasonality = {
+				displayMonths: season.replace('(southern)', '').trim()
+			};
+		} else if (season.includes('(northern and southern)')) {
+			jsonData[id].nSeasonality = {
+				displayMonths: season.replace('(northern and southern)', '').trim()
+			};
+			jsonData[id].sSeasonality = {
+				displayMonths: season.replace('(northern and southern)', '').trim()
+			};
+		}
 	});
 
-	fs.writeFile(`./${key}.json`, JSON.stringify(listData, null, '\t'), console.log);
+	jsonData[id].nSeasonality.displayMonths = formatDisplayMonths(jsonData[id].nSeasonality.displayMonths);
+	jsonData[id].sSeasonality.displayMonths = formatDisplayMonths(jsonData[id].sSeasonality.displayMonths);
+
+	jsonData[id].nSeasonality.activeMonths = getActiveMonths(jsonData[id].nSeasonality.displayMonths);
+	jsonData[id].sSeasonality.activeMonths = getActiveMonths(jsonData[id].sSeasonality.displayMonths);
 });
+
+fs.writeFile('./sea-creatures.json', JSON.stringify(jsonData, null, '\t'), console.log);
